b7ae794f84adae1eb84fae50f840c5c75e0d8420
VicCont <vic.contraerads@gmail.com>
2022-12-15 12:32:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pyspark 
-from pyspark.sql import SparkSession
+#import pyspark 
+#from pyspark.sql import SparkSession
 import re
 import os
 from logit import Logit
@@ -9,6 +9,7 @@ import json
 import pickle
 import numpy as np
 import hashlib
+import multiprocessing
 
 def save_model(dir_model,model):
     pickle.dump(model, open('model.pkl','wb'))
@@ -18,7 +19,6 @@ def load_model(dir_model):
     if (os.path.isfile(dir_model)):
         return pickle.load(open('model.pkl','rb'))
     return None
-
 
 def get_training_data(dir_training_data,stop_words):
     if not (os.path.isfile(dir_training_data)):
@@ -52,44 +52,61 @@ def get_stop_words(direccion_stopwords):
         return stop_words
     else:
         return pd.read_pickle(direccion_stopwords)
-direccion_stopwords="stopword.pickle"
-dir_training_data="training_data.pickle"
-pattern=re.compile(r"\s{2.}")
-stop_words=get_stop_words(direccion_stopwords)
-stop_words=set(stop_words[0].to_list())
-training_data=get_training_data(dir_training_data,stop_words)
-prepare_strings=lambda x: set(re.sub(r" +", r" ", x.translate(str.maketrans('', '', string.punctuation))).split())
-training_data=training_data.assign(context_jaccard=lambda df: list((list([prepare_strings(z)-stop_words for z in y]) for y in (x for x in df["context"].array))))
-training_data=training_data.assign(questions_jaccard=lambda df: list((list([prepare_strings(z)-stop_words  for z in y]) for y in (x for x in df["questions"].array))))
-training_data=training_data.explode(["context","context_jaccard"])
-training_data=training_data[training_data["context_jaccard"] != set()]
-training_data["len_cont"]= training_data["context"].str.len()
-training_data["suma_acumulada"]=training_data.groupby(['hash_contexto'])["len_cont"].cumsum()-training_data['len_cont']
-training_data["suma_posterior"]=training_data.groupby('hash_contexto')['suma_acumulada'].shift(-1)
-training_data=training_data.drop(['len_cont',"context",'questions'],axis=1)
-training_data=training_data.explode(['questions_jaccard','index_respuesta'])
-training_data=training_data[training_data["index_respuesta"].notna()]
-training_data['index_respuesta'] = training_data['index_respuesta'].astype('int')
-training_data['responds_to_question']=(training_data.suma_acumulada<=training_data.index_respuesta) & (((training_data.suma_posterior>training_data.index_respuesta) | (training_data.suma_posterior.isnull())) )
-training_data=training_data.drop(['suma_acumulada','suma_posterior','index_respuesta','hash_contexto'],axis=1)
-training_data=training_data.assign(jaccard=lambda df: list(len(x.intersection(y))/len(x.union(y)) for x,y in zip(df['questions_jaccard'],df['context_jaccard'])))#training_data.set_index([['questions','index_respuesta','hash_contexto']])
-training_data=training_data.drop(['context_jaccard','questions_jaccard'],axis=1)
+ 
+def bootstraping(X,y):
+    model=Logit(X, y)
+    model.train()
+    return model.theta
+    
 
-#spark = SparkSession.builder.appName('Proyecto2FDD.com').getOrCreate()
-#spark.sparkContext.setLogLevel('ERROR')
-#training_data=pd.concat([suma_acumulada.set_index(['hash_contexto','context']),suma_acumulada.set_index(['hash_contexto','context'])], axis=1, join='inner')
-## training_data["context"].str.replace(f"[{string.punctuation}]*",'',regex=True) quita puntuacion
+def append_results(result):
+    result_list.append(result)
 
-modelo =load_model("model.pkl")
-if (not modelo):
+if __name__=="__main__":
+    pool = multiprocessing.Pool(processes=10)
+
+    direccion_stopwords="stopword.pickle"
+    dir_training_data="training_data.pickle"
+    pattern=re.compile(r"\s{2.}")
+    stop_words=get_stop_words(direccion_stopwords)
+    stop_words=set(stop_words[0].to_list())
+    training_data=get_training_data(dir_training_data,stop_words)
+    prepare_strings=lambda x: set(re.sub(r" +", r" ", x.translate(str.maketrans('', '', string.punctuation))).split())
+    training_data=training_data.assign(context_jaccard=lambda df: list((list([prepare_strings(z)-stop_words for z in y]) for y in (x for x in df["context"].array))))
+    training_data=training_data.assign(questions_jaccard=lambda df: list((list([prepare_strings(z)-stop_words  for z in y]) for y in (x for x in df["questions"].array))))
+    training_data=training_data.explode(["context","context_jaccard"])
+    training_data=training_data[training_data["context_jaccard"] != set()]
+    training_data["len_cont"]= training_data["context"].str.len()
+    training_data["suma_acumulada"]=training_data.groupby(['hash_contexto'])["len_cont"].cumsum()-training_data['len_cont']
+    training_data["suma_posterior"]=training_data.groupby('hash_contexto')['suma_acumulada'].shift(-1)
+    training_data=training_data.drop(['len_cont',"context",'questions'],axis=1)
+    training_data=training_data.explode(['questions_jaccard','index_respuesta'])
+    training_data=training_data[training_data["index_respuesta"].notna()]
+    training_data['index_respuesta'] = training_data['index_respuesta'].astype('int')
+    training_data['responds_to_question']=(training_data.suma_acumulada<=training_data.index_respuesta) & (((training_data.suma_posterior>training_data.index_respuesta) | (training_data.suma_posterior.isnull())) )
+    training_data=training_data.drop(['suma_acumulada','suma_posterior','index_respuesta','hash_contexto'],axis=1)
+    training_data=training_data.assign(jaccard=lambda df: list(len(x.intersection(y))/len(x.union(y)) for x,y in zip(df['questions_jaccard'],df['context_jaccard'])))#training_data.set_index([['questions','index_respuesta','hash_contexto']])
+    training_data=training_data.drop(['context_jaccard','questions_jaccard'],axis=1)
+
+    #spark = SparkSession.builder.appName('Proyecto2FDD.com').getOrCreate()
+    #spark.sparkContext.setLogLevel('ERROR')
+
+    ##modelo =load_model("model.pkl")
+    ##if (not modelo):
+    training_data=training_data.sample(frac=0.05,random_state=123456,axis=0,ignore_index=True)
+    repeticiones=100
     modelo=Logit(training_data["jaccard"].to_numpy().reshape(-1, 1) ,training_data["responds_to_question"].to_numpy().reshape(-1, 1) )
     modelo.train()
-    save_model('model.pkl', modelo)
-
-input()
-pregunta=input("Ingresa tu preguta: ")
-pregunta=pregunta.lower()
-pregunta=re.sub(pattern, r" ", pregunta)
-oraciones=pregunta.split(".")
-oraciones=[x.strip().split(" ") for x in oraciones]
-print(oraciones)
+    original_theta=modelo.theta
+    ##save_model('model.pkl', modelo)
+    bootstraping_thetas=np.array([])
+    result_list=[]
+    for _ in range(repeticiones):
+        aux_data=training_data.sample(frac=1,random_state=123456,axis=0,ignore_index=True)
+        pool.apply_async(bootstraping,[aux_data["jaccard"].to_numpy().reshape(-1, 1) ,aux_data["responds_to_question"].to_numpy().reshape(-1, 1)],callback=append_results)
+    pool.close()
+    pool.join()
+    print(bootstraping_thetas)
+    bootstraping_thetas=np.append(bootstraping_thetas,result_list)
+    bootstraping_thetas=bootstraping_thetas.reshape(repeticiones,2)
+    print(bootstraping_thetas)
